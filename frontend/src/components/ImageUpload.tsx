1eab6929360f9eb0fb2c903d03c71db0b904a073
NaidaNozic <naida.nozic@reeinvent.com>
2025-03-19 18:20:34 +0100

    Added delete button in image uploader

--- frontend/src/components/ImageUpload.tsx
+++ frontend/src/components/ImageUpload.tsx
@@ -1,6 +1,7 @@
 import './ImageUpload.css';
 import React, { useState } from 'react';
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 interface ImageUploadProps {
   onDetectIssues: (base64Image: string) => void;
@@ -9,6 +10,7 @@ interface ImageUploadProps {
 const ImageUploader: React.FC<ImageUploadProps> = ({ onDetectIssues }) => {
   const [image, setImage] = useState<string | null>(null);
   const [base64Image, setBase64Image] = useState<string | null>(null);
+  const [imageUploaded, setImageUploaded] = useState<boolean>(false);
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -18,28 +20,47 @@ const ImageUploader: React.FC<ImageUploadProps> = ({ onDetectIssues }) => {
         const base64String = reader.result?.toString().split(',')[1] || '';
         setBase64Image(base64String);
         setImage(URL.createObjectURL(file));
+        setImageUploaded(true);
         onDetectIssues(base64String);
       };
       reader.readAsDataURL(file);
     }
   };
 
+  const handleImageRemove = () => {
+    setImage(null);
+    setBase64Image(null);
+    setImageUploaded(false);
+  };
+
   return (
     <div className='ImageUpload-Container'>
-      <Typography variant="h6">Upload an Image</Typography>
-      <input
-        type="file"
-        accept="image/*"
-        style={{ display: 'none' }}
-        id="upload-button"
-        onChange={handleImageUpload}
-      />
-      <label htmlFor="upload-button">
-        <Button variant="contained" component="span">
-          Choose Image
-        </Button>
-      </label>
-      {image && <img src={image} alt="Uploaded preview" className='ImageUpload-Preview' />}
+      {!imageUploaded && (
+        <>
+          <input
+            type="file"
+            accept="image/*"
+            style={{ display: 'none' }}
+            id="upload-button"
+            onChange={handleImageUpload}
+          />
+          <label htmlFor="upload-button">
+            <Button variant="contained" component="span">
+              Choose Image
+            </Button>
+          </label>
+        </>
+      )}
+
+      {image && (
+        <div>
+          <IconButton onClick={handleImageRemove} style={{marginLeft: '95%'}}>
+            <DeleteIcon />
+          </IconButton>
+
+          <img src={image} alt="Uploaded preview" className='ImageUpload-Preview' />
+        </div>
+      )}
     </div>
   );
 };
