4225043d2876ce836626bdc0c7b203c86e7cb949
NaidaNozic <naida.nozic@reeinvent.com>
2025-03-21 17:28:13 +0100

    Modified styling of remove image button

--- frontend/src/components/ImageUpload.tsx
+++ frontend/src/components/ImageUpload.tsx
@@ -1,7 +1,9 @@
 import './ImageUpload.css';
 import React, { useState } from 'react';
-import { Button, Typography, IconButton } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { css } from '@emotion/react';
+import { styled } from '@mui/material/styles';
 
 interface ImageUploadProps {
   onDetectIssues: (base64Image: string) => void;
@@ -34,7 +36,7 @@ const ImageUploader: React.FC<ImageUploadProps> = ({ onDetectIssues }) => {
   };
 
   return (
-    <div className='ImageUpload-Container'>
+    <div className={`ImageUpload-Container ${imageUploaded ? 'uploaded' : ''}`}>
       {!imageUploaded && (
         <>
           <input
@@ -53,16 +55,44 @@ const ImageUploader: React.FC<ImageUploadProps> = ({ onDetectIssues }) => {
       )}
 
       {image && (
-        <div>
-          <IconButton onClick={handleImageRemove} style={{marginLeft: '95%'}}>
-            <DeleteIcon />
-          </IconButton>
+        <div className="ImageUpload-Wrapper">
+          <StyledButton
+            variant="contained"
+            startIcon={<DeleteIcon />}
+            onClick={handleImageRemove} >
+            Remove
+          </StyledButton>
 
-          <img src={image} alt="Uploaded preview" className='ImageUpload-Preview' />
+          <StyledText variant="subtitle1" className="ImageUpload-Text">
+            Application Screen: Usability issues are detected from the uploaded image.
+          </StyledText>
+
+          <img src={image} alt="Uploaded preview" className="ImageUpload-Preview" />
         </div>
       )}
     </div>
   );
 };
+
+const StyledButton = styled(Button)`
+  background-color: white;
+  color: #494a4a;
+  border-radius: 20px;
+  padding: 6px 16px;
+  border: 1px solid white;
+  font-weight: bold;
+  text-transform: none;
+  transition: box-shadow 0.2s ease-in-out;
+  box-shadow: none;
+
+  &:hover {
+    box-shadow: 0px 2px 8px rgba(198, 35, 35, 0.1);
+    background-color: white;
+  }
+`;
+
+const StyledText = styled(Typography)`
+  color: #878787;
+`;
 
 export default ImageUploader;
